13e814f0d30b702f153e3c91870727378a7c98f2
Patrick Ewing <patrick.henry.ewing@gmail.com>
2012-12-14 20:06:37 -0800

    Two tests

--- notation_spec.js
+++ notation_spec.js
@@ -3,11 +3,25 @@ var Parser = require('parser');
 
 describe('Drone Notation', function() {
 
-    it('should parse the ➚ character as a "takeoff" action.', function() {
-      expect(Parser.parse('➚')).toEqual(".takeoff();")
-    });
+  it('should parse the ➚ character as a "takeoff" action.', function() {
+    expect(Parser.parse('➚')).toEqual(".takeoff();")
+  });
 
-    it('should parse multiple characters as multiple chained calls', function() {
-      expect(Parser.parse('⧖ ⧖ ⧖')).toEqual(".wait()\n.wait()\n.wait();")
-    });
+  it('should parse multiple characters as multiple chained calls', function() {
+    expect(Parser.parse('⧖ ⧖ ⧖')).toEqual(".wait()\n.wait()\n.wait();")
+  });
+
+  it('should parse multiple, idiosyncratic chars', function() {
+    var expectedOutput = ""+<r><![CDATA[
+      .takeoff()
+      .right()
+      .forward()
+      .back()
+      .up
+      .land();
+    ]]></r>;
+
+    expect(Parser.parse('➚ → ↑ ↓ △ ➘')).toEqual(expectedOutput);
+  });
+
 });
