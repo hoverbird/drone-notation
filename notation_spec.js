173651ddf2d601d184b03228887d632db9999a0d
Patrick Ewing <patrick.henry.ewing@gmail.com>
2012-12-14 20:01:13 -0800

    Two tests

--- notation_spec.js
+++ notation_spec.js
@@ -3,18 +3,11 @@ var Parser = require('parser');
 
 describe('Drone Notation', function() {
 
-    // var _order = new Order([ new OrderItem(12), new OrderItem(16) ]),
-    //     _totalAmountWithoutDiscount = _order.getTotalAmount();
-    //     _customer = new Customer([ _order ]);
-
-    // _customer.makePreferred();
-
     it('should parse the ➚ character as a "takeoff" action.', function() {
-        console.log(Parser)
-        expect(Parser.parse('➚')).toEqual("client.takeoff();")
+      expect(Parser.parse('➚')).toEqual(".takeoff();")
     });
 
-    // it('should apply a ten percent discount to all outstanding orders', function() {
-    //     _order.getTotalAmount().should.equal(_totalAmountWithoutDiscount * 0.9);
-    // });
+    it('should parse multiple characters as multiple chained calls', function() {
+      expect(Parser.parse('⧖ ⧖ ⧖')).toEqual(".wait()\n.wait()\n.wait();")
+    });
 });
--- parser.js
+++ parser.js
@@ -1,7 +1,18 @@
 exports.parse = function (input) {
-  if (input === '➚') {
-    return "client.takeoff();"
-  } else {
-    return "fuck you"
+  var tokenized = input.split(' ');
+  var output = [];
+  var i = 0;
+
+  while (i <= tokenized.length) {
+    var char = tokenized[i];
+
+    if        (char === '⧖') {
+      output.push('.wait()')
+    } else if (char === '➚') {
+      output.push('.takeoff()');
+    }
+    i++
   }
+
+  return output.join('\n') + ';';
 }
